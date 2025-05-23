3f8a8fdf85a3456735b4a5a83efd07af02af3347
DariaAleshina <ddaleshina@gmail.com>
2025-05-23 18:06:04 +0200

    timer & highliting answer state wip

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,11 +1,14 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 import CompleteView from './CompleteView';
 import Timer from './Timer';
 import QUESTIONS from '../questions';
+const TIMER_DURATION = 5000;
 
 export default function Quiz() {
+  const [answerState, setAnswerState] = useState('');
   const [userAnswers, setUserAnswers] = useState([]);
-  const activeQuestionIndex = userAnswers.length;
+  const activeQuestionIndex =
+    answerState === '' ? userAnswers.length : userAnswers.length - 1;
 
   const quizIsComplete = QUESTIONS.length === activeQuestionIndex;
   if (quizIsComplete) {
@@ -15,28 +18,68 @@ export default function Quiz() {
   const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers].sort(
     () => Math.random() - 0.5
   );
-  const handleAnswerClick = function (selectedAnswer) {
-    setUserAnswers(prevAnswers => {
-      return [...prevAnswers, selectedAnswer];
-    });
-  };
+
+  const handleAnswerClick = useCallback(
+    function handleAnswerClick(selectedAnswer) {
+      setAnswerState('answered');
+      setTimeout(() => {
+        if (selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]) {
+          setAnswerState('correct');
+        } else {
+          setAnswerState('wrong');
+        }
+
+        setTimeout(() => {
+          setAnswerState('');
+        }, 2000);
+      }, 1000);
+
+      setUserAnswers(prevAnswers => {
+        return [...prevAnswers, selectedAnswer];
+      });
+    },
+    [activeQuestionIndex]
+  );
+
+  const handleTimeoutExpire = useCallback(
+    () => handleAnswerClick(null),
+    [handleAnswerClick]
+  );
 
   return (
     <div id="quiz">
       <div id="question">
+        <Timer
+          key={QUESTIONS[activeQuestionIndex].id}
+          timeout={TIMER_DURATION}
+          onTimeout={handleTimeoutExpire}
+        />
         <h2>
           {QUESTIONS[activeQuestionIndex].id.toUpperCase() +
             ': ' +
             QUESTIONS[activeQuestionIndex].text}
         </h2>
         <ul id="answers">
-          {shuffledAnswers.map(answer => (
-            <li key={answer} className="answer">
-              <button onClick={() => handleAnswerClick(answer)}>
-                {answer}
-              </button>
-            </li>
-          ))}
+          {shuffledAnswers.map(answer => {
+            const isSelected = userAnswers[userAnswers.length - 1] === answer;
+            let buttonStyling = '';
+            if (isSelected) {
+              if (answerState === 'answered') buttonStyling = 'selected';
+              if (answerState === 'correct' || answerState === 'wrong')
+                buttonStyling = answerState;
+            }
+
+            return (
+              <li key={answer} className="answer">
+                <button
+                  onClick={() => handleAnswerClick(answer)}
+                  className={buttonStyling}
+                >
+                  {answer}
+                </button>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </div>
--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,3 +1,26 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
-export default function Timer() {}
+export default function Timer({ timeout, onTimeout }) {
+  const [remainingTime, setRemainingTime] = useState(timeout);
+  useEffect(() => {
+    console.log('timer set!');
+    const timer = setTimeout(onTimeout, timeout);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [timeout, onTimeout]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      console.log('interval set!');
+      setRemainingTime(prevRemainingTime => prevRemainingTime - 100);
+    }, 100);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return <progress id="question-time" max={timeout} value={remainingTime} />;
+}
