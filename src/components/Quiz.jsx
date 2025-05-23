80674f790575b575ea0c23da71e6e27ba3e4f249
DariaAleshina <ddaleshina@gmail.com>
2025-05-23 13:41:52 +0200

    added Header component

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+
+import QUESTIONS from '../questions';
+
+export default function Quiz() {
+  const [userAnswers, setUserAnswers] = useState([]);
+  const activeQuestionIndex = userAnswers.length;
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers].sort(
+    () => Math.random() - 0.5
+  );
+
+  const handleAnswerClick = function (selectedAnswer) {
+    setUserAnswers(prevAnswers => {
+      return [...prevAnswers, selectedAnswer];
+    });
+  };
+
+  return (
+    <div id="quiz">
+      <div id="question">
+        <h2>
+          {QUESTIONS[activeQuestionIndex].id.toUpperCase() +
+            ': ' +
+            QUESTIONS[activeQuestionIndex].text}
+        </h2>
+        <ul id="answers">
+          {shuffledAnswers.map(answer => (
+            <li key={answer} className="answer">
+              <button onClick={() => handleAnswerClick(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
